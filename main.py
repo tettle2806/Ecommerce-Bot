b6b3905cc0619bb950daa5d6d21be0a30b5dce11
tettle2806 <zidanzohid@gmail.com>
2023-10-17 10:44:53 +0500

    del

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from utils.notify_admin import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 import middlewares
-from handlers.admin.activate_middlewares import check_status
+
 
 
 async def on_startup(dp):
     middlewares.setup(dp)
-    await check_status(dp)
     await on_startup_notify(dp)
     await set_default_commands(dp)
     print('Бот запущен')
